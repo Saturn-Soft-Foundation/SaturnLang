a69e5bb9dc7dc141d1a324075a74f75d518cb450
timofey_arkhipov <timar@hotmail.com>
2023-11-26 23:59:27 +0300

    implement pos struct

--- compiler/frontend/saturnc_lexer/src/lib.rs
+++ compiler/frontend/saturnc_lexer/src/lib.rs
@@ -1,6 +1,8 @@
 mod token;
 mod token_tag;
+mod pos;
 
+pub use pos::*;
 pub use token_tag::*;
 pub use token::*;
 
--- compiler/frontend/saturnc_lexer/src/token.rs
+++ compiler/frontend/saturnc_lexer/src/token.rs
@@ -1,7 +1,9 @@
+use crate::pos::Pos;
+use crate::token_tag::TokenTag;
+
 #[derive(Clone, Copy, Debug)]
 pub struct Token {
     pub tag: TokenTag,
-    // TODO: Implement spans for better diagnostics
-    // pub span: Span
+    pub pos: Pos
 }
 
